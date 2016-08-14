e4bf9c5287e3b6571aaccb046b3f5d613f364037
Skysec <hecber@gmail.com>
2016-08-14 02:40:07 +0000

    Add Instance filtering to obtain Public IP Address

--- aws-app.sh
+++ aws-app.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
 AMI="ami-2d39803a"
-KEY_PAIR="hecber_keys"
 SECURITY_GROUP="hecber_sg"
 INSTANCE_NAME="hecber_wave"
 APP_PORT="8080"
 INSTANCE_TYPE="t2.micro"
 USER_DATA="user_data.sh"
 
+# Check if ACCESS KEY ID has been defined
 if [ -z ${AWS_ACCESS_KEY_ID} ]; then
   echo "Please set and export AWS_ACCESS_KEY_ID"
   exit 2
 fi
-
+# Check if SECRET ACCESS KEy has been defined
 if [ -z ${AWS_SECRET_ACCESS_KEY} ]; then
   echo "Please set and export AWS_SECRET_ACCESS_KEY"
   exit 2
@@ -21,7 +21,6 @@ export AWS_ACCESS_KEY_ID
 export AWS_SECRET_ACCESS_KEY
 export AWS_DEFAULT_REGION=${AWS_DEFAULT_REGION-"us-east-1"}
 export AMI
-export KEY_PAIR
 export SECURITY_GROUP
 export INSTANCE_NAME
 export APP_PORT
@@ -30,19 +29,24 @@ export USER_DATA
 
 # Create Security Group, port 8000 must be open
 aws ec2 create-security-group --group-name "${SECURITY_GROUP}" \
---description "${SECURITY_GROUP}"
+--description "${SECURITY_GROUP}" >/dev/null 2>&1
 
 # Add Rules to the Security Group
 aws ec2 authorize-security-group-ingress --group-name "${SECURITY_GROUP}" \
---protocol tcp --port "${APP_PORT}" --cidr 0.0.0.0/0
+--protocol tcp --port "${APP_PORT}" --cidr 0.0.0.0/0 >/dev/null 2>&1
 
 # Run instance
-aws ec2 run-instances --image-id "${AMI}" --count 1 \
---key-name "${INSTANCE_NAME}" \
+INSTANCE_ID=`aws ec2 run-instances --image-id "${AMI}" --count 1 \
 --security-groups "${SECURITY_GROUP}" \
 --instance-type "${INSTANCE_TYPE}" \
---user-data file://"${USER_DATA}"
+--user-data file://"${USER_DATA}" | grep InstanceId | egrep -o "i\-[0-9a-f]+"`
 
 # Get Public Ip Address
-IP_ADDR=`aws ec2 describe-instances --filter "Name=key-name,Values=${INSTANCE_NAME}" | grep PublicIpAddress | egrep -o "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+"`
-echo $IP_ADDR
+IP_ADDR=`aws ec2 describe-instances \
+--filter "Name=instance-id,Values=${INSTANCE_ID}" | grep PublicIpAddress | egrep -o "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+"`
+
+# Give some time to provision
+sleep 25
+
+# Output URL
+echo "http://${IP_ADDR}:${APP_PORT}"
--- user_data.sh
+++ user_data.sh
@@ -3,7 +3,7 @@ apt-add-repository ppa:ansible/ansible -y
 apt-get update -y
 apt-get install ansible -y
 apt-get install git -y
-mkdir /usr/loca/repo
-git clone https://https://github.com/skysec/aws-ansible-lab.git /usr/local/repo
-cd /usr/loca/repo/
+mkdir /usr/local/repo
+git clone https://github.com/skysec/aws-ansible-lab.git /usr/local/repo
+cd /usr/local/repo/playbook
 ansible-playbook site.yml
